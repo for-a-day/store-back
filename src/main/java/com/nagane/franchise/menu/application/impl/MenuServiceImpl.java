f590e3114d2c1c5f057fd6e23908f43368a38f2c
dollyishere <rundollyrun8@gmail.com>
2024-06-27 16:49:21 +0900

    feat: 도메인 모두 추가

--- src/main/java/com/nagane/franchise/menu/application/impl/MenuServiceImpl.java
+++ src/main/java/com/nagane/franchise/menu/application/impl/MenuServiceImpl.java
@@ -1,7 +1,10 @@
 package com.nagane.franchise.menu.application.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.nagane.franchise.menu.application.MenuService;
 
+@Service
 public class MenuServiceImpl implements MenuService {
 
 }
