321ada41f8455d4d202a6667ba623ff7257d1769
dollyishere <rundollyrun8@gmail.com>
2024-06-27 15:36:18 +0900

    feat: first setting

---
+++ src/main/java/com/nagane/franchise/FranchiseBackApplication.java
@@ -0,0 +1,13 @@
+package com.nagane.franchise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranchiseBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranchiseBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nagane/franchise/FranchiseBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagane.franchise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
