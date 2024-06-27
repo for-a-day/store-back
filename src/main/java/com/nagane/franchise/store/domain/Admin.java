64691e0362c496bc25b3b04176b1f880583604eb
KCCI119\kcci <kcci@kcci119>
2024-06-27 18:52:00 +0900

    feat: menu, category, admin 엔티티 세팅

--- src/main/java/com/nagane/franchise/store/domain/Admin.java
+++ src/main/java/com/nagane/franchise/store/domain/Admin.java
@@ -1,5 +1,35 @@
 package com.nagane.franchise.store.domain;
 
-public class Admin {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+/**
+ * @author ljy
+ * @since 2024.06.27
+ * Admin entity 코드
+ * **/
+@Entity
+@Table(name = "admin")
+@SequenceGenerator(name = "admin_seq", sequenceName = "admin_seq", allocationSize = 1)
+@Data
+public class Admin {
+	
+	/* 관리자 번호(pk) */
+	@Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "admin_seq")
+    private Long adminNo;
+	
+	/* 관리자 아이디 */
+	@Column(name = "admin_id", nullable = false, length = 50)
+    private String adminId;
+	
+	/* 관리자 비밀번호 */
+	@Column(name = "admin_password", nullable = false)
+    private String adminPassword;
 }
