1fdb9a13f7dbeabea0c24c90f86550958964d0e0
dollyishere <rundollyrun8@gmail.com>
2024-06-27 19:59:56 +0900

    feat: 주문-메뉴 연계 엔티티 등 구현 및 기존 엔티티들 관계 수정

--- src/main/java/com/nagane/franchise/menu/domain/Menu.java
+++ src/main/java/com/nagane/franchise/menu/domain/Menu.java
@@ -1,5 +1,10 @@
 package com.nagane.franchise.menu.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.nagane.franchise.order.domain.OrderMenu;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -8,16 +13,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 /**
- * @author ljy
- * @since 2024.06.27
  * Menu entity 코드
- * **/
+ * 
+ * @autor ljy
+ * @since 2024.06.27
+ **/
 @Entity
 @Table(name = "menu")
 @SequenceGenerator(name = "menu_seq", sequenceName = "menu_seq", allocationSize = 1)
@@ -30,40 +36,39 @@ public class Menu {
 	private Long menuNo;
 	
 	/* 메뉴명 */
-	@Column(name="menu_name", nullable = false, length=100)
+	@Column(name = "menu_name", nullable = false, length = 100)
 	private String menuName;
 	
 	/* 메뉴 코드 */
-	@Column(name="menu_id", nullable = false, length=100)
+	@Column(name = "menu_id", nullable = false, length = 100)
 	private String menuId;
 	
 	/* 판매가격 */
-	@Column(name="price", nullable = false)
+	@Column(name = "price", nullable = false)
 	private Integer price;
 	
 	/* 이미지 */
-	@Column(name="menu_img", length=255)
+	@Column(name = "menu_img", length = 255)
 	private String menuImage;
 	
 	/* 설명 */
-	@Column(name="description", length=500)
+	@Column(name = "description", length = 500)
 	private String description;
 	
 	/* 카테고리 번호(fk) */
-	@ManyToOne(targetEntity = Category.class, fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_no", insertable = false, updatable = false)
-	private Long categoryNo;
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_no", nullable = false)
+	private Category category;
 	
 	/* 재료 공급가 */
-	@Column(name="supply_price", nullable = false)
+	@Column(name = "supply_price", nullable = false)
 	private Integer supplyPrice;
 	
 	/* 상태(0=단종, 1=판매) */
-	@Column(name="state", nullable=false)
+	@Column(name = "state", nullable = false)
 	private Integer state = 1;
 	
-	/*
-	 * @PrePersist protected void onCreate() { if (this.state == null) { this.state
-	 * = 1; // 기본 값 1로 설정 } }
-	 */
+	/* order_menu 엔티티와 OneToMany 매핑 */
+	@OneToMany(mappedBy = "menu", fetch = FetchType.LAZY)
+	private List<OrderMenu> orderMenuList = new ArrayList<>();
 }
