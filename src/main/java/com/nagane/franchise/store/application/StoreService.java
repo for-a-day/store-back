c1e87ff1cc63a455aa6cc6737e582bceef393c29
dollyishere <rundollyrun8@gmail.com>
2024-06-28 17:31:17 +0900

    feat: admin 로직 수정, store 컨트롤러 구현, stoke service impl 잘못 implements된 거 수정

--- src/main/java/com/nagane/franchise/store/application/StoreService.java
+++ src/main/java/com/nagane/franchise/store/application/StoreService.java
@@ -1,5 +1,11 @@
 package com.nagane.franchise.store.application;
 
+import java.util.List;
+
+import com.nagane.franchise.store.dto.store.StoreCreateDto;
+import com.nagane.franchise.store.dto.store.StoreDto;
+import com.nagane.franchise.store.dto.store.StoreLoginDto;
+
 /**
  * @author ljy
  * @since 2024.06.28
@@ -7,5 +13,20 @@ package com.nagane.franchise.store.application;
  * 지점 관련 service 인터페이스
  * **/
 public interface StoreService {
+	
+	/** 지점 목록 조회 */
+	List<StoreDto> getStoreList();
+	
+	/** 지점 신규 등록(지점장 회원 가입) */
+	void createStore(StoreCreateDto storeCreateDto);
+	
+	/** 지점 정보 수정 */
+	void updateStore(StoreDto storeUpdateDto);
+	
+	/** 지점 삭제 */
+	void deleteStore(Long StoreNo);
+	
+	/** 가맹점 로그인 */
+	void loginStore(StoreLoginDto storeLoginDto);
 	
 }
