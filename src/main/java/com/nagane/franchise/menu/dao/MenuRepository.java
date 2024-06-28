8f8e99b1cc960ac6e2485d3fdbed6f7b8e360409
dollyishere <rundollyrun8@gmail.com>
2024-06-28 15:09:09 +0900

    fix: menu 엔티티의 menu_id => menu_code로 수정

--- src/main/java/com/nagane/franchise/menu/dao/MenuRepository.java
+++ src/main/java/com/nagane/franchise/menu/dao/MenuRepository.java
@@ -1,6 +1,7 @@
 package com.nagane.franchise.menu.dao;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -15,6 +16,9 @@ public interface MenuRepository extends JpaRepository<Menu, Long> {
 	
 	/* 카테고리 번호로 메뉴 리스트 반환 */
     List<Menu> findByCategory_CategoryNo(Long categoryNo);
+    
+    /* 메뉴 코드로 메뉴 존재하는지 여부 조회 */
+    Optional<Menu> findByMenuCode(String menuCode);
 	
 	/* 카테고리 번호와 상태값(1)로 해당하는 메뉴 리스트 반환 */
 	List<Menu> findByCategory_CategoryNoAndState(Long categoryNo, Integer state);
