4971ee30e5ea81130d99f1939096f96f45d30b56
dollyishere <rundollyrun8@gmail.com>
2024-06-28 15:02:12 +0900

    feat: admin, store 기본 controller, service 의존성 주입

--- src/main/java/com/nagane/franchise/store/application/StoreService.java
+++ src/main/java/com/nagane/franchise/store/application/StoreService.java
@@ -1,5 +1,11 @@
 package com.nagane.franchise.store.application;
 
+/**
+ * @author ljy
+ * @since 2024.06.28
+ * Store Service 코드
+ * 지점 관련 service 인터페이스
+ * **/
 public interface StoreService {
-
+	
 }
