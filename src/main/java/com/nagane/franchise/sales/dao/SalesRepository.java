736ad42e1a24cdc16c5c6c8483c4a7332a2bd3cb
dollyishere <rundollyrun8@gmail.com>
2024-06-28 11:59:35 +0900

    feat: 브랜치 분리 작업 전 각 레포지토리에 커스텀 메서드(쿼리)들 사전 세팅

--- src/main/java/com/nagane/franchise/sales/dao/SalesRepository.java
+++ src/main/java/com/nagane/franchise/sales/dao/SalesRepository.java
@@ -1,9 +1,21 @@
 package com.nagane.franchise.sales.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nagane.franchise.sales.domain.Sales;
 
+/**
+ * @author ljy, nsr
+ * @since 2024.06.28
+ * 매출 관련 repository 코드
+ * **/
 public interface SalesRepository extends JpaRepository<Sales, Long>{
-
+	
+	/* 가맹점 번호, 년도, 월로 해당하는 매출 레코드 반환 */
+	List<Sales> findByStoreAndYearAndMonth(Long storeNo, Integer year, Integer month);
+	
+	/* 년도, 월로 해당하는 매출 레코드 반환 */
+	List<Sales> findByYearAndMonth(Integer year, Integer month);
 }
--- src/main/java/com/nagane/franchise/store/dao/AdminRepository.java
+++ src/main/java/com/nagane/franchise/store/dao/AdminRepository.java
@@ -4,6 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nagane.franchise.store.domain.Admin;
 
+/**
+ * @author ljy, nsr
+ * @since 2024.06.28
+ * 관리자 관련 repository 코드
+ * **/
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-
+	
+	/* 관리자 아이디와 비밀번호로 해당하는 레코드 반환 */
+	Admin findByAdminIdAndAdminPassword(String adminId, String adminPassword);
+	
 }
