323465b46ce73f6aa15bd7e63f0025beafce21d6
dollyishere <rundollyrun8@gmail.com>
2024-07-01 09:24:14 +0900

    fix: 주석 변경

--- src/main/java/com/nagane/franchise/table/api/TableController.java
+++ src/main/java/com/nagane/franchise/table/api/TableController.java
@@ -1,5 +1,32 @@
 package com.nagane.franchise.table.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nagane.franchise.table.application.TableService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author ljy
+ * @since 2024.06.28
+ * Table controller 코드
+ * 테이블 오더 관련 controller
+ * **/
+@Tag(name= "테이블 오더 API")
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:${pos.port}")
 public class TableController {
+	
+	// 의존성 주입 (di)
+	@Autowired
+	private TableService tableService;
+	
+	/**
+	 * 
+	 * */
 
 }
