7be30eb8cc5de486f45ba3b4c95a7fb1e8ba6391
dollyishere <rundollyrun8@gmail.com>
2024-07-02 10:05:40 +0900

    feat: 테이블 삭제, 업데이트 추가

--- src/main/java/com/nagane/franchise/table/application/TableService.java
+++ src/main/java/com/nagane/franchise/table/application/TableService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import com.nagane.franchise.table.dto.TableAdminDto;
 import com.nagane.franchise.table.dto.TableLoginDto;
+import com.nagane.franchise.table.dto.TableNoDto;
 import com.nagane.franchise.table.dto.TableResponseDto;
+import com.nagane.franchise.table.dto.TableUpdateDto;
 
 /**
  * @author ljy
@@ -19,6 +21,12 @@ public interface TableService {
 	
 	/** 테이블 신규 등록 */
 	void createTable(Long storeNo);
+	
+	/** 테이블 수정 */
+	void updateTable(TableUpdateDto tableUpdateDto);
+	
+	/** 테이블 삭제 */
+	void deleteTable(Long tableNo);
 	
 	/** 테이블 오더 로그인 */
 	void loginTable(TableLoginDto tableLoginDto);
