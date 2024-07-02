b642280b5900396cad53a27930358151501500d7
dollyishere <rundollyrun8@gmail.com>
2024-07-02 15:18:04 +0900

    refactor : swagger 및 data 따로 담는 걸 담당하는 entity로 responseBody class 총 3 가지로 분리

--- src/main/java/com/nagane/franchise/util/model/response/BaseResponseBody.java
+++ src/main/java/com/nagane/franchise/util/model/response/BaseResponseBody.java
@@ -1,7 +1,6 @@
 package com.nagane.franchise.util.model.response;
 
-import java.util.Map;
-
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 /**
@@ -11,44 +10,27 @@ import lombok.Data;
  * 서버 요청 기본 응답값
  **/
 @Data
+@Schema(description = "응답용 response")
 public class BaseResponseBody {
-	// 상태 코드 (예시: 200)
-	Integer statusCode = null;
-	// 응답 메시지 (예시: success)
-	String message = null;
-	// 전달할 데이터
-	Map<String, Object> data = null;
-	
-	public BaseResponseBody() {}
-	
-	public BaseResponseBody(Integer statusCode) {
-		this.statusCode = statusCode;
-	}
-	
-	public BaseResponseBody(Integer statusCode, String message) {
-		this.statusCode = statusCode;
-		this.message = message;
-	}
-	
-	public BaseResponseBody(Integer statusCode, String message, Map<String, Object> data) {
-		this.statusCode = statusCode;
-		this.message = message;
-	}
-	
-	// 팩토리 패턴 기반으로 객체 생성하는 유틸리티 메서드
-	public static BaseResponseBody of(Integer statusCode, String message) {
-		BaseResponseBody body = new BaseResponseBody();
-		body.message = message;
-		body.statusCode = statusCode;
-		return body;
-	}
-	
-	public static BaseResponseBody of(Integer statusCode, String message, Map<String, Object> data) {
-		BaseResponseBody body = new BaseResponseBody();
-		body.message = message;
-		body.statusCode = statusCode;
-		body.data = data;
-		return body;
-	}
-
+    @Schema(description = "상태 코드", nullable = false, example="200")
+    Integer statusCode = null;
+    
+    @Schema(description = "응답 메시지", nullable = false, example="Success")
+    String message = null;
+    
+    public BaseResponseBody() {}
+    
+    public BaseResponseBody(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
+    
+    public BaseResponseBody(Integer statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+    
+    // 팩토리 패턴 기반으로 객체 생성하는 유틸리티 메서드
+    public static BaseResponseBody of(Integer statusCode, String message) {
+        return new BaseResponseBody(statusCode, message);
+    }
 }
