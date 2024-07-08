a843200dfaf48c579954730d101d8d354f363f42
NaSoRim <lasolim@naver.com>
2024-07-08 18:29:01 +0900

    feat : 세큐리디 jwt 적용

---
+++ src/main/java/com/nagane/franchise/global/config/AuthenticationFilter.java
@@ -0,0 +1,88 @@
+package com.nagane.franchise.global.config;
+
+import java.io.IOException;
+import com.nagane.franchise.global.config.JwtUtil;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.nagane.franchise.store.application.impl.AdminServiceImpl;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+
+@Component
+public class AuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final AdminServiceImpl adminLoginService;
+
+    @Autowired
+    public AuthenticationFilter(JwtUtil jwtUtil, AdminServiceImpl adminLoginService) {
+        this.jwtUtil = jwtUtil;
+        this.adminLoginService = adminLoginService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7);
+            System.out.println("Extracted token: " + token); // 토큰 출력
+
+            try {
+                Claims claims = jwtUtil.parseToken(token);
+                String adminId = claims.getSubject();
+
+                CustomUserDetails userDetails = new CustomUserDetails(adminId.toString(), "",
+                        AuthorityUtils.NO_AUTHORITIES, adminId);
+
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+                // Access Token 갱신 로직
+//                if (jwtUtil.isTokenExpired(token)) {
+//                    String refreshToken = request.getHeader("Refresh-Token");
+//                    if (refreshToken != null) {
+//                        // Validate refresh token
+//                        Claims refreshClaims = jwtUtil.parseToken(refreshToken);
+//                        String adminIdFromRefresh = refreshClaims.getSubject();
+//
+//                        if (adminIdFromRefresh != null && adminIdFromRefresh.equals(adminId)) {
+//                            // Generate new access token
+//                            String newToken = jwtUtil.generateToken(adminId);
+//                            response.setHeader("Authorization", "Bearer " + newToken);
+//                        }
+//                    }
+//                }
+
+            } catch (ExpiredJwtException e) {
+                logger.error("JWT Token has expired: ", e);
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("JWT Token has expired");
+                return;
+            } catch (Exception e) {
+                logger.error("JWT Token validation error: ", e);
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("JWT Token validation error");
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
